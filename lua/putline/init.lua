a4f2d298b09695fef0bc15cb6fa0eb50f3b82268
Runeword <cha.cascio@gmail.com>
2022-05-11 20:10:39 +0200

    refactor(init.lua): add more verbose variables names

--- lua/putline/init.lua
+++ lua/putline/init.lua
@@ -15,14 +15,14 @@ M.setup = function(opts)
 	    exe 'normal! "'.v:register.a:how
 	    call setreg(v:register, body, type)
 	  endif
-	  silent! call repeat#set("\<Plug>(unimpaired-put-".a:map.")")
+    " silent! call repeat#set("\<Plug>(unimpaired-put-".a:map.")")
 	endfunction
 
 	nnoremap <silent> <Plug>(unimpaired-put-above-reformat)  :<C-U>call <SID>putline(v:count1 . 'gP', 'Above')<CR>=']
 	nnoremap <silent> <Plug>(unimpaired-put-below-reformat)  :<C-U>call <SID>putline(v:count1 . 'gp', 'Below')<CR>=']
 	  ]])
-	map("n", "gP", "<Plug>(unimpaired-put-above-reformat)g$:set ve= ve=all<CR>")
-	map("n", "gp", "<Plug>(unimpaired-put-below-reformat)g$:set ve= ve=all<CR>")
+	map("n", "gP", "<Plug>(unimpaired-put-above-reformat)g$:set virtualedit= virtualedit=all<CR>")
+	map("n", "gp", "<Plug>(unimpaired-put-below-reformat)g$:set virtualedit= virtualedit=all<CR>")
 end
 
 return M
