ba3445b6049c82e2e399928af24f1a4ad4e9ac26
Runeword <cha.cascio@gmail.com>
2022-05-12 19:17:55 +0200

    refactor(init.lua): convert vimscript code to lua

--- lua/putline/init.lua
+++ lua/putline/init.lua
@@ -1,28 +1,34 @@
-local cmd = vim.cmd
-local map = vim.keymap.set
+local v = vim.v
+local f = vim.fn
 
 local M = {}
 
-M.setup = function(opts)
-	-- print("lua/putline.lua")
-	cmd([[
-	 function! s:putline(how, map) abort
-	  let [body, type] = [getreg(v:register), getregtype(v:register)]
-	  if type ==# 'V'
-	    exe 'normal! "'.v:register.a:how
-	  else
-	    call setreg(v:register, body, 'l')
-	    exe 'normal! "'.v:register.a:how
-	    call setreg(v:register, body, type)
-	  endif
-    " silent! call repeat#set("\<Plug>(unimpaired-put-".a:map.")")
-	endfunction
+local function put(keys)
+	local name = v.register
+	local contents = f.getreg(name)
+	local type = f.getregtype(name)
+	local linewise = "l"
+	local isLinewise = type == "V"
 
-	nnoremap <silent> <Plug>(unimpaired-put-above-reformat)  :<C-U>call <SID>putline(v:count1 . 'gP', 'Above')<CR>=']
-	nnoremap <silent> <Plug>(unimpaired-put-below-reformat)  :<C-U>call <SID>putline(v:count1 . 'gp', 'Below')<CR>=']
-	  ]])
-	map("n", "gP", "<Plug>(unimpaired-put-above-reformat)g$:set virtualedit= virtualedit=all<CR>")
-	map("n", "gp", "<Plug>(unimpaired-put-below-reformat)g$:set virtualedit= virtualedit=all<CR>")
+	if isLinewise then
+		print('normal! "' .. name .. keys)
+		f.execute('normal! "' .. name .. keys)
+	else
+		f.setreg(name, contents, linewise)
+		f.execute('normal! "' .. name .. keys)
+		f.setreg(name, contents, type)
+	end
+end
+
+M.above = function() put("]P") end
+M.below = function() put("]p") end
+
+M.setup = function(opts)
+	-- nnoremap <silent> <Plug>(unimpaired-put-above-reformat)  :<C-U>call <SID>putline(v:count1 . 'gP', 'Above')<CR>=']
+	-- nnoremap <silent> <Plug>(unimpaired-put-below-reformat)  :<C-U>call <SID>putline(v:count1 . 'gp', 'Below')<CR>=']
+	--   ]])
+	-- map("n", "gP", "<Plug>(unimpaired-put-above-reformat)g$:set virtualedit= virtualedit=all<CR>")
+	-- map("n", "gp", "<Plug>(unimpaired-put-below-reformat)g$:set virtualedit= virtualedit=all<CR>")
 end
 
 return M
