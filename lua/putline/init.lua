c29f56fb2d27a9f06b786bc1f046805b2ab646a6
Runeword <cha.cascio@gmail.com>
2022-05-12 21:21:44 +0200

    feat(init.lua): add command count

--- lua/putline/init.lua
+++ lua/putline/init.lua
@@ -1,34 +1,34 @@
 local v = vim.v
 local f = vim.fn
+-- local map = vim.keymap.set
 
 local M = {}
 
 local function put(keys)
+	local count = v.count
 	local name = v.register
 	local contents = f.getreg(name)
 	local type = f.getregtype(name)
-	local linewise = "l"
-	local isLinewise = type == "V"
+	local linewise = "V"
 
-	if isLinewise then
-		print('normal! "' .. name .. keys)
-		f.execute('normal! "' .. name .. keys)
+	-- print('normal! ' .. count .. '"' .. name .. keys .. '`]')
+
+	if type == linewise then
+		f.execute("normal! " .. count .. '"' .. name .. keys .. "`]")
 	else
 		f.setreg(name, contents, linewise)
-		f.execute('normal! "' .. name .. keys)
-		f.setreg(name, contents, type)
+		f.execute("normal! " .. count .. '"' .. name .. keys .. "`]")
 	end
 end
 
-M.above = function() put("]P") end
-M.below = function() put("]p") end
-
-M.setup = function(opts)
-	-- nnoremap <silent> <Plug>(unimpaired-put-above-reformat)  :<C-U>call <SID>putline(v:count1 . 'gP', 'Above')<CR>=']
-	-- nnoremap <silent> <Plug>(unimpaired-put-below-reformat)  :<C-U>call <SID>putline(v:count1 . 'gp', 'Below')<CR>=']
-	--   ]])
-	-- map("n", "gP", "<Plug>(unimpaired-put-above-reformat)g$:set virtualedit= virtualedit=all<CR>")
-	-- map("n", "gp", "<Plug>(unimpaired-put-below-reformat)g$:set virtualedit= virtualedit=all<CR>")
+M.above = function()
+	put("]P")
 end
+M.below = function()
+	put("]p")
+end
+M.setup = function(opts) end
+
+-- map("n", "S", function() require('putline').below() end)
 
 return M
