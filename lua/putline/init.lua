64a8bc9fab23fdfc8b1853b9dda1d734729e65c8
Runeword <cha.cascio@gmail.com>
2022-05-15 15:36:16 +0200

    refactor(init.lua)

--- lua/putline/init.lua
+++ lua/putline/init.lua
@@ -5,42 +5,42 @@ local f = vim.fn
 local M = {}
 
 local function putLinewise(keys)
-	local register =  {}
+	local register = {}
 	register.name = v.register
 	register.contents = f.getreg(register.name)
 	register.type = f.getregtype(register.name)
 	local linewise = "V"
 	local motion = "`]"
-  local count = v.count1
+	local count = v.count1
 
 	if register.type ~= linewise then
 		f.setreg(register.name, register.contents, linewise)
-  end
-		f.execute("normal! " .. count .. '"' .. register.name .. keys .. motion)
+	end
+	f.execute("normal! " .. count .. '"' .. register.name .. keys .. motion)
 end
 
 local function putCharwise(keys)
-  local register =  {}
+	local register = {}
 	register.name = v.register
 	register.contents = f.getreg(register.name)
 	register.type = f.getregtype(register.name)
 	local linewise = "V"
 	local charwise = "v"
-  local count = v.count1
+	local count = v.count1
 
 	if register.type == linewise then
 		-- Remove spaces at both extremities
 		local str = string.gsub(register.contents, "^%s*(.-)%s*$", "%1")
 		f.setreg(register.name, str, charwise)
 	end
-  f.execute("normal! " .. count .. '"' .. register.name .. keys)
+	f.execute("normal! " .. count .. '"' .. register.name .. keys)
 end
 
 M.putCharwiseAfter = function()
-  putCharwise("p")
+	putCharwise("p")
 end
 M.putCharwiseBefore = function()
-  putCharwise("P")
+	putCharwise("P")
 end
 M.putLinewiseAbove = function()
 	putLinewise("]P")
