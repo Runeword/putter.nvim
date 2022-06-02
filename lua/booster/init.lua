2cd9120089b3ad0285ce8f39af0aa44773af492a
Runeword <cha.cascio@gmail.com>
2022-06-02 19:01:30 +0200

    feat(putCharwise): add prefix and suffix params

--- lua/booster/init.lua
+++ lua/booster/init.lua
@@ -7,70 +7,74 @@ local cmd = vim.cmd
 local M = {}
 
 M.putLinewise = function(command, surround)
-  local register = {}
-  register.name = v.register
-  register.contents = fn.getreg(register.name)
-  register.type = fn.getregtype(register.name)
-  local linewise = "V"
-  local count = v.count1
-  local str = ''
-  local prefix
-  local suffix
+  return function()
+    local register = {}
+    register.name = v.register
+    register.contents = fn.getreg(register.name)
+    register.type = fn.getregtype(register.name)
+    local linewise = "V"
+    local count = v.count1
+    local str = ''
+    local prefix
+    local suffix
 
-  if (surround) then
-    local input = fn.getcharstr()
-    prefix = input
-    suffix = input
+    if (surround) then
+      local input = fn.getcharstr()
+      prefix = input
+      suffix = input
+    end
+
+    -- if register.type ~= linewise then
+
+    -- Add prefix and suffix
+    str = (prefix or '') .. register.contents .. (suffix or '')
+
+    fn.setreg(register.name, str, linewise)
+    fn.execute("normal! " .. count .. '"' .. register.name .. command)
+    fn.setreg(register.name, register.contents, register.type)
   end
-
-  -- if register.type ~= linewise then
-
-  -- Add prefix and suffix
-  str = (prefix or '') .. register.contents .. (suffix or '')
-
-  fn.setreg(register.name, str, linewise)
-  fn.execute("normal! " .. count .. '"' .. register.name .. command)
-  fn.setreg(register.name, register.contents, register.type)
 end
 
-M.putCharwise = function(command, surround)
-  local linewise = "V"
-  local charwise = "v"
-  local count = v.count1
-  local register = {}
-  local str = ''
-  local prefix
-  local suffix
+M.putCharwise = function(command, prefix, suffix)
+  return function()
+    local linewise = "V"
+    local charwise = "v"
+    local count = v.count1
+    local register = {}
+    local str = ''
 
-  -- fn.inputsave()
-  -- local input = fn.input('> ')
-  -- prefix = input
-  -- suffix = input
-  -- fn.inputrestore()
+    -- fn.inputsave()
+    -- local input = fn.input('> ')
+    -- prefix = input
+    -- suffix = input
+    -- fn.inputrestore()
 
-  if (surround) then
-    local input = fn.getcharstr()
-    prefix = input
-    suffix = input
+    if (prefix and suffix) then
+      local input = fn.getcharstr()
+      prefix = input
+      suffix = input
+    elseif (prefix) then prefix = fn.getcharstr()
+    elseif (suffix) then suffix = fn.getcharstr()
+    end
+
+    register.name = v.register
+    register.type = fn.getregtype(register.name)
+    register.contents = fn.getreg(register.name)
+
+    if register.type == linewise then
+      -- Remove spaces at both extremities
+      str = string.gsub(register.contents, "^%s*(.-)%s*$", "%1")
+    else
+      str = register.contents
+    end
+
+    -- Add prefix and suffix
+    str = (prefix or '') .. str .. (suffix or '')
+
+    fn.setreg(register.name, str, charwise)
+    fn.execute("normal! " .. count .. '"' .. register.name .. command)
+    fn.setreg(register.name, register.contents, register.type)
   end
-
-  register.name = v.register
-  register.type = fn.getregtype(register.name)
-  register.contents = fn.getreg(register.name)
-
-  if register.type == linewise then
-    -- Remove spaces at both extremities
-    str = string.gsub(register.contents, "^%s*(.-)%s*$", "%1")
-  else
-    str = register.contents
-  end
-
-  -- Add prefix and suffix
-  str = (prefix or '') .. str .. (suffix or '')
-
-  fn.setreg(register.name, str, charwise)
-  fn.execute("normal! " .. count .. '"' .. register.name .. command)
-  fn.setreg(register.name, register.contents, register.type)
 end
 
 M.addBuffersToQfList = function()
