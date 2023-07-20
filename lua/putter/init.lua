ce52dd707e20ff8d9215ef52812580d6778870e5
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-20 23:21:26 +0200

    refactor: remove unneded functions and files

--- lua/putter/init.lua
+++ lua/putter/init.lua
@@ -1,25 +1,12 @@
 local vim = vim
-local v = vim.v
-local o = vim.o
-local fn = vim.fn
-local cmd = vim.cmd
-local tbl_deep_extend = vim.tbl_deep_extend
--- vim.pretty_print(buffers)
 
--------------------- Put
 local M = {}
-
-local opts = require("putter.opts")
-
-M.setup = function(userOpts)
-  opts = tbl_deep_extend("force", opts, userOpts or {})
-end
 
 local function getRegister(command)
   local register = {}
-  register.name = command:match('^"(.)') or v.register
-  register.contents = fn.getreg(register.name)
-  register.type = fn.getregtype(register.name)
+  register.name = command:match('^"(.)') or vim.v.register
+  register.contents = vim.fn.getreg(register.name)
+  register.type = vim.fn.getregtype(register.name)
   return register
 end
 
@@ -27,9 +14,9 @@ local function putLinewise(command)
   local register = getRegister(command)
   local str = register.contents
 
-  fn.setreg(register.name, str, "V")                                    -- Set register linewise
-  fn.execute("normal! " .. v.count1 .. '"' .. register.name .. command) -- Put register
-  fn.setreg(register.name, register.contents, register.type)            -- Restore register
+  vim.fn.setreg(register.name, str, "V")                                        -- Set register linewise
+  vim.fn.execute("normal! " .. vim.v.count1 .. '"' .. register.name .. command) -- Put register
+  vim.fn.setreg(register.name, register.contents, register.type)                -- Restore register
 end
 
 local function putCharwise(command)
@@ -38,7 +25,7 @@ local function putCharwise(command)
 
   -- If register type is blockwise-visual then put as usual
   if register.type ~= "V" and register.type ~= "v" then
-    fn.execute("normal! " .. v.count1 .. '"' .. register.name .. command)
+    vim.fn.execute("normal! " .. vim.v.count1 .. '"' .. register.name .. command)
     return
   end
 
@@ -49,9 +36,9 @@ local function putCharwise(command)
     str = register.contents
   end
 
-  fn.setreg(register.name, str, "v")                                    -- Set register charwise
-  fn.execute("normal! " .. v.count1 .. '"' .. register.name .. command) -- Put register
-  fn.setreg(register.name, register.contents, register.type)            -- Restore register
+  vim.fn.setreg(register.name, str, "v")                                        -- Set register charwise
+  vim.fn.execute("normal! " .. vim.v.count1 .. '"' .. register.name .. command) -- Put register
+  vim.fn.setreg(register.name, register.contents, register.type)                -- Restore register
 end
 
 function M.putCharwise(command)
@@ -60,87 +47,6 @@ end
 
 function M.putLinewise(command)
   return function() putLinewise(command) end
-end
-
-local function isEndOfWord()
-  local pos = vim.fn.getpos('.')
-  vim.print(pos)
-  vim.fn.execute("normal! gee")
-  vim.print(vim.fn.getpos('.'))
-  if table.concat(pos) == table.concat(vim.fn.getpos('.')) then
-    return true
-  else
-    vim.fn.setpos('.', pos)
-    return false
-  end
-end
-
-local function isStartOfWord()
-  local pos = vim.fn.getpos('.')
-  vim.print(pos)
-  vim.fn.execute("normal! gew")
-  vim.print(vim.fn.getpos('.'))
-  if table.concat(pos) == table.concat(vim.fn.getpos('.')) then
-    return true
-  else
-    vim.fn.setpos('.', pos)
-    return false
-  end
-end
-
-function M.putWordwise()
-  return function()
-    local command
-    if isStartOfWord() and not isEndOfWord() then
-      command = 'P'
-    else
-      command = 'p'
-    end
-    putCharwise(command)
-  end
-end
-
--------------------- Snap
-
-local function isPastEndOfLine()
-  return (o.virtualedit ~= '') and (fn.col('.') >= fn.col('$'))
-end
-
-local function isBeforeFirstNonBlank()
-  return (o.virtualedit ~= '') and (fn.col(".") <= string.find(fn.getline(fn.line(".")), "(%S)") - 1)
-end
-
-function M.snapToLineStart(callback)
-  return function()
-    if isBeforeFirstNonBlank() then fn.execute('normal! ^') end
-    if type(callback) == 'string' then fn.execute('normal! ' .. callback) else callback() end
-  end
-end
-
-function M.snapToLineEnd(callback)
-  return function()
-    if isPastEndOfLine() then fn.execute('normal! $') end
-    if type(callback) == 'string' then fn.execute('normal! ' .. callback) else callback() end
-  end
-end
-
--------------------- Jump
-
-local function jumpToLine(command, callback)
-  if isPastEndOfLine() or isBeforeFirstNonBlank() then fn.execute('normal! ' .. command) end
-  if type(callback) == 'string' then fn.execute('normal! ' .. callback) else callback() end
-end
-
-function M.jumpToLine(command, callback)
-  return function() jumpToLine(command, callback) end
-end
-
-function M.jumpToLineStart(callback)
-  return function() jumpToLine('^', callback) end
-end
-
-function M.jumpToLineEnd(callback)
-  return function() jumpToLine('$', callback) end
 end
 
 return M
